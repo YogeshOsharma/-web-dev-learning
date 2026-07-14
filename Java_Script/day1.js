b0846e9c8fc3973943c8246df94dc469a7c5cfbf
YogeshOsharma <ys72610300@gmail.com>
2026-07-14 20:13:07 +0530

    Introduction to java script

---
+++ Java_Script/day1.js
@@ -0,0 +1,17 @@
+console.log("hello world");
+
+console.log("This is my day 1 of learning Java Script");
+
+console.log("My name is Yogesh");
+
+console.log("I am learning JavaScript");
+
+console.log("I study in JUET Guna");
+
+console.log("My favourite footballer is Mbappe");
+
+console.log("JavaScript is fun");
+
+console.log("Day 1 completed");
+
+console.log("See you tomorrow");
