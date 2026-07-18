80f27273937c7faebfe9a87227888292c887f1be
YogeshOsharma <ys72610300@gmail.com>
2026-07-18 20:39:16 +0530

    learned about data types

---
+++ Java_Script/day2.js
@@ -0,0 +1,16 @@
+console.log("Day 2 of learning java script");
+
+let name = "yogesh"
+console.log(typeof name)
+
+let num = 10
+console.log(typeof num)
+
+let food
+console.log(typeof food)
+
+let value = null
+console.log(typeof value)
+
+let number = true
+console.log(typeof number)
