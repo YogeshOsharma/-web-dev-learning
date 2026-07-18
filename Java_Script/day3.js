53e696e1c7f576c540ad51036c68ae8c6dc41fdc
YogeshOsharma <ys72610300@gmail.com>
2026-07-18 21:27:17 +0530

    learned about diffrent types of operators

---
+++ Java_Script/day3.js
@@ -0,0 +1,43 @@
+
+
+let a = 10;
+let b = 20;
+console.log(a+b);
+
+let c = 20;
+let d = 20;
+console.log(c-d);
+
+let e = 2;
+let f = 4;
+console.log(e*f);
+
+let g = 16;
+let h = 4;
+console.log(g/h);
+
+let i = 8;
+let j = 4;
+console.log(i%j);
+
+let num = 2;
+console.log(num ** 4)
+
+//Assingment operators
+
+let value = 10;
+
+let num1 = 10;
+num1 += 5;
+console.log(num1)
+
+//similarly there is -= , *= and /=
+
+//Comparison opertor 
+
+console.log(a==b); //sirf value compare karta hai
+
+let num2 = 10;
+let num3 = "10";
+console.log(num2===num3); //value aur type dono comapre karta hai
+
